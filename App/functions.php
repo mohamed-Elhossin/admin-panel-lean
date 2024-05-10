15370554e5a730a47ab7a6b0dff28a40a88eeac1
mohamed-Elhossin <m.m.m.elhossin@gmail.com>
2024-05-10 14:46:27 +0300

    first commite

--- App/functions.php
+++ App/functions.php
@@ -27,16 +27,102 @@ window.location.replace('http://localhost/learn/admin/$var');
 }
 
 
-function auth($rule2 = null , $rule3 = null ,$rule4 = null)
+function auth($rule2 = null, $rule3 = null, $rule4 = null)
 {
     if ($_SESSION['admin']) {
-        if ($_SESSION['admin']['rule'] == 1 || $_SESSION['admin']['rule']==$rule2
-        || $_SESSION['admin']['rule']==$rule4
-        || $_SESSION['admin']['rule']==$rule3) {
+        if (
+            $_SESSION['admin']['rule'] == 1 || $_SESSION['admin']['rule'] == $rule2
+            || $_SESSION['admin']['rule'] == $rule4
+            || $_SESSION['admin']['rule'] == $rule3
+        ) {
         } else {
             redirect('pages-error-404.php');
         }
     } else {
         redirect('pages-login.php');
+    }
+}
+
+
+// filter All Inputs
+function filterInputs($input)
+{
+    $input  = trim($input);
+    $input  = strip_tags($input);
+    $input  = stripslashes($input);
+    $input  = htmlspecialchars($input);
+    return $input;
+}
+
+
+// String Validation;
+
+function stringValidation($input, $min = 3, $max = 20)
+{
+    $emptyError = empty($input);
+    $minError = strlen($input) < $min;
+    $maxError = strlen($input) >  $max;
+    if ($emptyError == true || $minError == true || $maxError == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Number Validation
+
+function numberValidation($input, $numberStatus = 'int')
+{
+    $emptyError = empty($input);
+    $isNagtiveError = $input <= 0;
+    if ($numberStatus == 'int') {
+        $isNumberError = filter_var($input, FILTER_VALIDATE_INT) == false;
+    } else {
+        $isNumberError = filter_var($input, FILTER_VALIDATE_FLOAT) == false;
+    }
+
+    if ($emptyError == true || $isNagtiveError == true || $isNumberError == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function fileReq($file_name)
+{
+    $emptyError = empty($file_name);
+    if ($emptyError == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sizeFile($fileSize, $your_size = 2)
+{
+    $sizeByMiga = ($fileSize / 1024) / 1024;
+    if ($sizeByMiga > $your_size) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validationType(
+    $file_type,
+    $type1 = null,
+    $type2 = null,
+    $type3 = null,
+    $type4 = null,
+    $type5 = null,
+) {
+    if (
+        $file_type == $type1 || $file_type == $type2 ||
+        $file_type == $type3 || $file_type == $type4 ||
+        $file_type == $type5
+    ) {
+        return false;
+    } else {
+        return true;
     }
 }
--- courses/create.php
+++ courses/create.php
@@ -7,14 +7,53 @@ include_once '../shared/head.php';
 include_once '../shared/header.php';
 include_once '../shared/aside.php';
 $message = null;
+$errors = [];
+$name = null;
 if (isset($_POST['send'])) {
-    $name = $_POST['name'];
-    $teacher = $_POST['teacher'];
-    $category = $_POST['category'];
 
-    $insert = "INSERT INTO courses VALUES (null , '$name' ,$category,$teacher)";
-    $i = mysqli_query($conn, $insert);
-    $message =  testMessage($i, "Create courses Successfully");
+
+    $name =  filterInputs($_POST['name']);
+    $teacher = filterInputs($_POST['teacher']);
+    $category = filterInputs($_POST['category']);
+    $description = filterInputs($_POST['description']);
+    $hours     = filterInputs($_POST['hours']);
+    if (stringValidation($name)) {
+        $errors[] = "Please Enter Valid Name";
+    }
+    if (stringValidation($description)) {
+        $errors[] = "Please Enter Valid Description";
+    }
+
+    if (numberValidation($hours)) {
+        $errors[] = "Please Enter  Valid Number in Hours";
+    }
+
+
+    // Image Code;
+    if (fileReq($_FILES['image']['name'])) {
+        $errors[] = "Please Enter Image";
+    }
+    $file_size = $_FILES['image']['size'];
+    if (sizeFile($file_size, 1)) {
+        $errors[] = "Please Enter Image Size Leess than";
+    }
+    $image_type = $_FILES['image']['type'];
+
+    if (validationType($image_type, 'image/jpeg', 'image/jpg', 'image/png')) {
+        $errors[] = "Please Enter  Valid Type Image";
+    }
+
+    $image_name = rand(0, 255) . rand(0, 255) .  $_FILES['image']['name'];
+    $image_tmp = $_FILES['image']['tmp_name'];
+    $location = "./upload/$image_name";
+    move_uploaded_file($image_tmp, $location);
+    // ?????
+    $full_image_path = URL('courses/upload/') . $image_name;
+    if (empty($errors)) {
+        $insert = "INSERT INTO courses VALUES (null , '$name' ,'$full_image_path' , '$hours' ,'$description',$category,$teacher)";
+        $i = mysqli_query($conn, $insert);
+        $message =  testMessage($i, "Create courses Successfully");
+    }
 }
 $categories = "SELECT * FROM categories";
 $categoriesdata  = mysqli_query($conn, $categories);
@@ -25,6 +64,18 @@ $teachersdata  = mysqli_query($conn, $teachers);
 <main id="main" class="main">
 
     <div class="card">
+        <?php
+
+
+        if (!empty($errors)) : ?>
+            <div class="alert alert-danger">
+                <?php foreach ($errors as $error) : ?>
+                    <ul>
+                        <li> <?= $error ?></li>
+                    </ul>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
         <?php if ($message != null) : ?>
             <div class="alert alert-success">
                 <?= $message ?>
@@ -37,9 +88,18 @@ $teachersdata  = mysqli_query($conn, $teachers);
             </h5>
 
             <!-- No Labels Form -->
-            <form method="post" class="row g-3">
+            <form method="post" class="row g-3" enctype="multipart/form-data">
                 <div class="col-md-12">
-                    <input type="text" name="name" class="form-control" placeholder="Teacher Name">
+                    <input type="text" name="name" value="<?=  $name ?>" class="form-control" placeholder="Course Name">
+                </div>
+                <div class="col-md-12">
+                    <input type="text" name="description" class="form-control" placeholder="Course description">
+                </div>
+                <div class="col-md-12">
+                    <input type="number" name="hours" class="form-control" placeholder="Course hours	">
+                </div>
+                <div class="col-md-12">
+                    <input type="file" name="image" class="form-control" placeholder="Course Image">
                 </div>
 
                 <div class="row">
